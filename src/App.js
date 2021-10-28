5c44308950d2a341c0e123d501c1a5c38ba3879f
Jayasurya Karthikeyan <jayasurya.23it@licet.ac.in>
2021-10-28 16:51:14 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    // <>
-    // <div>App page</div>
-    // </>
     <Router>
-      {/* <Button > boostrap </Button> */}
       <Switch>
-
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
